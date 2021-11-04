1591a3297215240e57fb865b83297e3b2324dd71
Rahul Chavan <51946480+Rahul168Chavan@users.noreply.github.com>
2021-11-04 18:20:26 +0530

    #1 Increased the time limit

--- Aioz.js
+++ Aioz.js
@@ -7,21 +7,21 @@ describe('Build Download :', ()  => {
       await $("//input[@id='email']").setValue(Email);
       await $("//input[@id='password']").setValue(Password);
       await $("//button[contains(text(),'Continue')]").click();
-      await browser.pause(5000);
+      await browser.pause(10000);
       await browser.saveScreenshot(ScreenShotPath+"1.png");
       await $("(.//*[@class='vjs-marker '])[1]").click();
       await browser.pause(3000)
-      await $("(.//*[@class='vjs-marker '])[2]").waitForDisplayed({ timeout: 50000 });
-      await browser.pause(5000)
+      await $("(.//*[@class='vjs-marker '])[2]").waitForDisplayed({ timeout: 80000 });
+      await browser.pause(3000)
       await browser.saveScreenshot(ScreenShotPath+"2.png");
       await $("(.//*[@class='vjs-marker '])[2]").click();
       await browser.pause(3000)
-      await $("(.//*[@class='vjs-marker '])[3]").waitForDisplayed({ timeout: 50000 });
-      await browser.pause(5000)
+      await $("(.//*[@class='vjs-marker '])[3]").waitForDisplayed({ timeout: 80000 });
+      await browser.pause(3000)
       await browser.saveScreenshot(ScreenShotPath+"3.png");
       await $("(.//*[@class='vjs-marker '])[3]").click();
       await browser.pause(3000)
-      await $("(.//*[@class='vjs-marker '])[1]").waitForDisplayed({ timeout: 50000 });
+      await $("(.//*[@class='vjs-marker '])[1]").waitForDisplayed({ timeout: 80000 });
       await browser.pause(1000)
     });
   });
