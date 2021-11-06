3cff0c7b9a8bdee82b884619b7a4bde174a2d889
RahulChavan <51946480+Rahul168Chavan@users.noreply.github.com>
2021-11-06 23:44:50 +0530

    looped the test case

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,9 +21,7 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    specs: [
-        './Aioz.js'
-    ],
+    specs: Array(200).fill('./Aioz.js'),
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
